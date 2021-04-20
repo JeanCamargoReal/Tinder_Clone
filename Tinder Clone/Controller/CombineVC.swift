66a8c652c61bae93a40c1aa5d7015308f97e8813
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-04-20 10:11:31 -0300

    Múltiplos cards e aplicando rotação

--- Tinder Clone/Controller/CombineVC.swift
+++ Tinder Clone/Controller/CombineVC.swift
@@ -14,19 +14,31 @@ class CombineVC: UIViewController {
         
         view.backgroundColor = UIColor.systemGroupedBackground
         
-        let redView = UIView()
-        redView.backgroundColor = .red
-        redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+        self.adicionarCards()
+    }
+}
+
+
+extension CombineVC {
+    
+    func adicionarCards() {
         
-        redView.center = view.center
-        
-        // Observador para quando o usuário clicar no card e arrastar.
-        let gesture = UIPanGestureRecognizer()
-        gesture.addTarget(self, action: #selector(handlerCard))
-        
-        redView.addGestureRecognizer(gesture)
-        
-        view.addSubview(redView)
+        for item in 1...3 {
+            
+            let redView = UIView()
+            redView.backgroundColor = item == 2 ? .blue : .red
+            redView.frame = CGRect(x: 0, y: 0, width: 200, height: 300)
+            
+            redView.center = view.center
+            
+            // Observador para quando o usuário clicar no card e arrastar.
+            let gesture = UIPanGestureRecognizer()
+            gesture.addTarget(self, action: #selector(handlerCard))
+            
+            redView.addGestureRecognizer(gesture)
+            
+            view.addSubview(redView)
+        }
     }
 }
 
@@ -34,15 +46,21 @@ extension CombineVC {
     
     @objc func handlerCard(_ gesture: UIPanGestureRecognizer) {
         if let card = gesture.view {
-            // pega a posição que o usuário está arrastando
+            // Pega a posição que o usuário está arrastando
             let point = gesture.translation(in: view)
             
             card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
             
-            // retorna o card para posição central
+            // Rotação do card
+            let rotationAngle = point.x / view.bounds.width * 0.4
+            
+            card.transform = CGAffineTransform(rotationAngle: rotationAngle)
+            
+            // Retorna o card para posição central
             if gesture.state == .ended {
                 UIView.animate(withDuration: 0.2) {
                     card.center = self.view.center
+                    card.transform = .identity
                 }
                 
                 
