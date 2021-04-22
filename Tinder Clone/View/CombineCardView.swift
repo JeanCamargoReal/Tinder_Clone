88844712678e601ac3c68aedb7c85e4cec309404
JeanCamargoReal <jeancamargoreal@icloud.com>
2021-04-22 09:33:40 -0300

    Adicionando Deslike e Like no card

--- Tinder Clone/View/CombineCardView.swift
+++ Tinder Clone/View/CombineCardView.swift
@@ -20,20 +20,14 @@ class CombineCardView: UIView {
         }
     }
     
-    let fotoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "pessoa-1")
-        // para imagem nao ficar esticada
-        imageView.contentMode = .scaleAspectFill
-        // cortar a imagem caso ultrappase o card
-        imageView.clipsToBounds = true
-        
-        return imageView
-    }()
+    let fotoImageView: UIImageView = .fotoImageView()
     
     let nomeLabel: UILabel = .textBoldLabel(32, textColor: .white)
     let idadeLabel: UILabel = .textLabel(28, textColor: .white)
     let fraseLabel: UILabel = .textLabel(18, textColor: .white, numberOfLines: 2)
+    
+    let deslikeImageView: UIImageView = .iconCard(named: "card-deslike")
+    let likeImageView: UIImageView = .iconCard(named: "card-like")
         
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -43,15 +37,30 @@ class CombineCardView: UIView {
         layer.cornerRadius = 8
         clipsToBounds = true
         
-        nomeLabel.text = "Ana Laura"
-        idadeLabel.text = "20"
-        fraseLabel.text = "O último a dar match chama."
-        
         nomeLabel.adicionaShadow()
         idadeLabel.adicionaShadow()
         fraseLabel.adicionaShadow()
             
         addSubview(fotoImageView)
+        
+        addSubview(deslikeImageView)
+        deslikeImageView.preencher(
+            top: topAnchor,
+            leading: nil,
+            trailing: trailingAnchor,
+            bottom: nil,
+            padding: .init(top: 20, left: 0, bottom: 0, right: 20)
+        )
+        
+        addSubview(likeImageView)
+        likeImageView.preencher(
+            top: topAnchor,
+            leading: leadingAnchor,
+            trailing: nil,
+            bottom: nil,
+            padding: .init(top: 20, left: 20, bottom: 0, right: 0)
+        )
+        
         
         fotoImageView.preencherSuperView()
         
